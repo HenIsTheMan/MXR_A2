7f04a79c30a9b0de14a2b87738223ddaaf3e4f01
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-15 21:41:26 +0800

    PlayerBehavior.cs (progress save)

--- Assets/Units/MXR/Scripts/Game/PlayerBehavior.cs
+++ Assets/Units/MXR/Scripts/Game/PlayerBehavior.cs
@@ -1,3 +1,4 @@
+using MobfishCardboard;
 using UnityEngine;
 
 namespace MXR {
@@ -6,6 +7,15 @@ namespace MXR {
 
         [SerializeField]
         private PlayerAttribs playerAttribs;
+
+        [SerializeField]
+        private Transform camTransform;
+
+        [SerializeField]
+        private float camDist;
+
+        [SerializeField]
+        private float camYOffset;
 
         #endregion
 
@@ -16,6 +26,11 @@ namespace MXR {
 
         internal PlayerBehavior(): base() {
             playerAttribs = null;
+
+            camTransform = null;
+
+            camDist = 0.0f;
+            camYOffset = 0.0f;
         }
 
         static PlayerBehavior() {
@@ -26,7 +41,27 @@ namespace MXR {
         #region Unity User Callback Event Funcs
 
         private void Update() {
-            transform.localPosition += Vector3.Normalize(transform.rotation * Vector3.forward) * playerAttribs.Spd;
+            Vector3 playerDir = Vector3.Normalize(transform.rotation * Vector3.forward);
+
+            transform.localPosition
+                += playerDir
+                * playerAttribs.Spd
+                * Time.deltaTime;
+
+            camTransform.localPosition = transform.localPosition - playerDir * camDist + new Vector3(0.0f, camYOffset, 0.0f);
+
+            camTransform.localRotation = Quaternion.FromToRotation(
+                Vector3.forward,
+                Vector3.Normalize(transform.localPosition - camTransform.localPosition)
+            );
+
+            //Vector3 eulerAngles = CardboardHeadTracker.trackerRawRotation.eulerAngles;
+            //transform.localRotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, eulerAngles.z);
+            //= Quaternion.Euler(
+            //    0.0f,
+            //    Mathf.Atan2((CardboardHeadTracker.trackerUnityRotation.eulerAngles * Vector3.forward).y, 1.0f),
+            //    0.0f
+            //);
         }
 
         #endregion
