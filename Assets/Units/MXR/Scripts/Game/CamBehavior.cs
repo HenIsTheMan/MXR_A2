8bc77025f692cd9eba47382226861e8647fc5fe8
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-16 14:56:42 +0800

    Revert "nah"
    This reverts commit a41129f38c64bdf45f15df4c69141bbe7a44f8c0.

--- Assets/Units/MXR/Scripts/Game/CamBehavior.cs
+++ Assets/Units/MXR/Scripts/Game/CamBehavior.cs
@@ -21,9 +21,6 @@ namespace MXR {
         private float camRotationSmoothingFactor;
 
         [SerializeField]
-        private bool shldSmoothOutCamRotationY;
-
-        [SerializeField]
         private PlayerAttribs playerAttribs;
 
         #endregion
@@ -42,8 +39,6 @@ namespace MXR {
             camPosSmoothingFactor = 0.0f;
             camRotationSmoothingFactor = 0.0f;
 
-            shldSmoothOutCamRotationY = true;
-
             playerAttribs = null;
         }
 
@@ -61,28 +56,14 @@ namespace MXR {
                 Time.deltaTime * camPosSmoothingFactor * playerAttribs.AccelFactor
             );
 
-            Quaternion rotation;
-            if(shldSmoothOutCamRotationY) {
-                rotation = Quaternion.Lerp(
-                    camTransform.localRotation,
-                    Quaternion.FromToRotation(
-                        Vector3.forward,
-                        Vector3.Normalize(playerAttribs.MyTransform.localPosition - camTransform.localPosition)
-                    ),
-                    Time.deltaTime * camRotationSmoothingFactor
-                );
-            } else {
-                Vector3 dir = Vector3.Normalize(playerAttribs.MyTransform.localPosition - camTransform.localPosition);
-
-                rotation = Quaternion.Lerp(
-                    camTransform.localRotation,
-                    Quaternion.FromToRotation(
-                        Vector3.forward,
-                        new Vector3(0.0f, dir.y, 0.0f)
-                    ),
-                    Time.deltaTime * camRotationSmoothingFactor
-                );
-            }
+            Quaternion rotation = Quaternion.Lerp(
+                camTransform.localRotation,
+                Quaternion.FromToRotation(
+                    Vector3.forward,
+                    Vector3.Normalize(playerAttribs.MyTransform.localPosition - camTransform.localPosition)
+                ),
+                Time.deltaTime * camRotationSmoothingFactor
+            );
 
             Vector3 eulerAngles = rotation.eulerAngles;
             rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, 0.0f);
