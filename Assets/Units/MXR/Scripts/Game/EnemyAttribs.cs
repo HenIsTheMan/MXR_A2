0d0232dc6fd3ada32ca7a8745d35deb4a63c079b
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-17 16:45:13 +0800

    EnemyAttribs.cs

---
+++ Assets/Units/MXR/Scripts/Game/EnemyAttribs.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace MXR {
+    internal sealed class EnemyAttribs: MonoBehaviour { //POD
+        #region Fields
+
+        private float currHealth;
+
+        #endregion
+
+        #region Properties
+
+        internal float CurrHealth {
+            get => currHealth;
+            set {
+                currHealth = value;
+            }
+        }
+
+        [field: SerializeField]
+        internal float MaxHealth {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal EnemyAttribs(): base() {
+            currHealth = 0.0f;
+            MaxHealth = 0.0f;
+        }
+
+        static EnemyAttribs() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            CurrHealth = MaxHealth;
+        }
+
+        #endregion
+    }
+}
