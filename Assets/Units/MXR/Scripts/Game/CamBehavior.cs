8ea56b48de934576b30376637fb788151666dbc5
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-17 09:15:14 +0800

    CamBehavior.cs (nope)

--- Assets/Units/MXR/Scripts/Game/CamBehavior.cs
+++ Assets/Units/MXR/Scripts/Game/CamBehavior.cs
@@ -50,27 +50,36 @@ namespace MXR {
         #region Unity User Callback Event Funcs
 
         private void FixedUpdate() {
-            camTransform.localPosition = Val.Lerp(
-                camTransform.localPosition,
-                playerAttribs.MyTransform.localPosition - playerAttribs.Dir * camHorizontalDist + new Vector3(0.0f, camYOffset, 0.0f),
-                Time.fixedDeltaTime * (camPosSmoothingFactor + playerAttribs.AccelFactor)
-            );
-
-            Quaternion rotation = Quaternion.Lerp(
-                camTransform.localRotation,
-                Quaternion.FromToRotation(
-                    Vector3.forward,
-                    Vector3.Normalize(playerAttribs.MyTransform.localPosition - camTransform.localPosition)
-                ),
-                Time.fixedDeltaTime * camRotationSmoothingFactor
-            );
-
-            Vector3 eulerAngles = rotation.eulerAngles;
-            rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, 0.0f);
-
-			camTransform.localRotation = rotation;
+            _ = StartCoroutine(nameof(UpdateCam));
         }
 
         #endregion
+
+        private System.Collections.IEnumerator UpdateCam() {
+            while(true) {
+                yield return new WaitForFixedUpdate();
+                //yield return new WaitForEndOfFrame(); //no need, makes it worse
+
+                camTransform.localPosition = Val.Lerp(
+                    camTransform.localPosition,
+                    playerAttribs.MyTransform.localPosition - playerAttribs.Dir * camHorizontalDist + new Vector3(0.0f, camYOffset, 0.0f),
+                    Time.fixedDeltaTime * (camPosSmoothingFactor + playerAttribs.AccelFactor)
+                );
+
+                Quaternion rotation = Quaternion.Lerp(
+                    camTransform.localRotation,
+                    Quaternion.FromToRotation(
+                        Vector3.forward,
+                        Vector3.Normalize(playerAttribs.MyTransform.localPosition - camTransform.localPosition)
+                    ),
+                    Time.fixedDeltaTime * camRotationSmoothingFactor
+                );
+
+                Vector3 eulerAngles = rotation.eulerAngles;
+                rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, 0.0f);
+
+                camTransform.localRotation = rotation;
+            }
+        }
     }
 }
