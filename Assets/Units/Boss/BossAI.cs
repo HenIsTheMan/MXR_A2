00c23ed01d5e6be62736ee2635e4aed0a4cebe61
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-18 10:11:51 +0800

    AnimationCurve[]

--- Assets/Units/Boss/BossAI.cs
+++ Assets/Units/Boss/BossAI.cs
@@ -13,13 +13,13 @@ namespace MXR {
         private float animDuration;
 
         [SerializeField]
-        private AnimationCurve xAnimCurve;
+        private AnimationCurve[] xAnimCurves;
 
         [SerializeField]
-        private AnimationCurve yAnimCurve;
+        private AnimationCurve[] yAnimCurves;
 
         [SerializeField]
-        private AnimationCurve zAnimCurve;
+        private AnimationCurve[] zAnimCurves;
 
         [SerializeField]
         private float xMultiplier;
@@ -43,9 +43,9 @@ namespace MXR {
             animTime = 0.0f;
             animDuration = 0.0f;
 
-            xAnimCurve = null;
-            yAnimCurve = null;
-            zAnimCurve = null;
+            xAnimCurves = System.Array.Empty<AnimationCurve>();
+            yAnimCurves = System.Array.Empty<AnimationCurve>();
+            zAnimCurves = System.Array.Empty<AnimationCurve>();
 
             xMultiplier = 0.0f;
             yMultiplier = 0.0f;
@@ -68,9 +68,14 @@ namespace MXR {
         private System.Collections.IEnumerator MoveBoss() {
             animTime = 0.0f;
             float time;
+
             Vector3 localPosOG = myTransform.localPosition;
             Quaternion localRotationOG = myTransform.localRotation;
             Vector3 lastLocalPos;
+
+            AnimationCurve xAnimCurve = xAnimCurves[Random.Range(0, xAnimCurves.Length)];
+            AnimationCurve yAnimCurve = yAnimCurves[Random.Range(0, yAnimCurves.Length)];
+            AnimationCurve zAnimCurve = zAnimCurves[Random.Range(0, zAnimCurves.Length)];
 
             while(animTime <= animDuration) {
                 animTime += Time.deltaTime;
